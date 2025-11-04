b5cefb226395dddf1c2833d8290bdaed4a5e84db
PaulN <paul@normingtons.org>
2025-11-03 20:41:03 -0800

    fixes launch script (again) (again) (again)

--- launch.sh
+++ launch.sh
@@ -2,6 +2,5 @@
 
 # add this launch script to ~/.bashrc (and remove the --test-mode)
 # a command like this should work
-# (cd /home/pi/rpi-ble; ./launch.sh)
 source venv/bin/activate
-python -m rpi_ble.main --test-mode
+python -m rpi_ble.main
